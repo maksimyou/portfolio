d1b6702e3bede9d9295ec950a60557107bd4957f
maksim ashmarin <maksimyou@gmail.com>
2023-01-09 12:48:57 +0300

    adding arrow anchor

--- my-app/src/RoutesComponent/WrapperRoutes.js
+++ my-app/src/RoutesComponent/WrapperRoutes.js
@@ -1,4 +1,4 @@
-
+import { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
 //import Main from './Main/Main';
@@ -7,12 +7,27 @@ import Footer from '../Footer/Footer';
 //import Header2 from './ToDoList/Header/Header';
 
 function WrapperRoutes() {
+    let [classArrow, setClassArrow] = useState(false);
+
+    function arrowShow() {
+        if (window.scrollY >= 1400) {
+            setClassArrow(true);
+        };
+        if (window.scrollY <= 1400) {
+            setClassArrow(false);
+        };
+    };
+    window.addEventListener('scroll', () => {
+        arrowShow();
+    });
+
 
     return (
         <>
             <Header />
             <Outlet />
             <Footer />
+            <a className={classArrow ? 'active-arrow' : 'arrow'} href='#root'><img src={require('../arrow.png')} /></a>
         </>
     );
 }
