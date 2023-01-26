0fcca5e51b4b2018fea5a43d96800d2e862b006a
maksim ashmarin <maksimyou@gmail.com>
2023-01-26 11:19:41 +0300

    Изменил название проекта

--- portfolio/src/Main/Projects/Projects.js
+++ portfolio/src/Main/Projects/Projects.js
@@ -4,8 +4,8 @@ function Projects() {
     return (
         <div id='6' className='projects_container' >
             <div className='projects_content'>
-                <div className='projects_item'><a href='/#'><img src={require('./Rectangle-1.png')} /></a></div>
-                <div className='projects_item'><a href='/#'><img src={require('./Rectangle-2.png')} /></a></div>
+                <div className='projects_item'><a target="_blank" href='https://maksimyou.github.io/Panteon'><img src={require('./project-1.png')} /></a></div>
+                <div className='projects_item'><a target="_blank" href='https://maksimyou.github.io/movies-react/'><img src={require('./project-2.png')} /></a></div>
                 <div className='projects_item'><a href='/#'><img src={require('./Rectangle-1.png')} /></a></div>
                 <div className='projects_item'><a href='/#'><img src={require('./Rectangle-2.png')} /></a></div>
                 <div className='projects_item'><a href='/#'><img src={require('./Rectangle-2.png')} /></a></div>
